560e2a6fc78856ab6d56212c93477d8001c3f242
liyanq528 <liyanq528@163.com>
2018-06-22 16:35:40 +0800

    add component

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+import { HighlightComponent } from './inspur-highlight/highlight.component';
+export declare class InspurHighlightModule {
+}
+export { HighlightComponent };
---
+++ inspur.highlight.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core"
+import { HighlightComponent } from "./inspur-highlight/highlight.component";
+
+@NgModule({
+  declarations: [HighlightComponent],
+  exports: [HighlightComponent]
+})
+export class InspurHighlightModule {
+
+}
